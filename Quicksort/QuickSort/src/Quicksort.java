9f216a46713a0e58000b5b9e8b948d59ee3f93bc
ziyaxu <ziyaxu07@gmail.com>
2018-01-05 22:47:33 -0500

    updated CSV writing format

--- Quicksort/QuickSort/src/Quicksort.java
+++ Quicksort/QuickSort/src/Quicksort.java
@@ -2,30 +2,250 @@ import java.util.Random;
 
 public class Quicksort {
 	
-	public static final int ARR_LENGTH = 10;
+	public static final int ARR_LENGTH = 10000;
 	public static final int ARR_RANGE = 1000;
-	public static final int REPEATS = 100;
+	public static final int REPEATS = 250;
 	
-    /* A utility function to print array of size n */
-    public static void printArray(int arr[])
+	public static final String LOMUTO = "Lomuto";
+	public static final String MIDDLE = "Middle";
+	public static final String MEAN = "Mean";
+	
+	private static int[] arr = new int[ARR_LENGTH];
+	
+    /**
+     * prints array
+     * @param arr
+     */
+    public void printArray()
     {
-        for (int i = 0; i < ARR_LENGTH; i++)
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
         System.out.println();
     }
     
-    
-    public static int[] ranArr() 
-    {
-    		int[] array = new int[ARR_LENGTH];
+    /**
+     * creates a random array of a specified length
+     * @return random array
+     */
+    public void ranArr() {
     		Random rand = new Random(); 
 
     		for (int i = 0; i < ARR_LENGTH; i++)
-    		{
-    			array[i] = rand.nextInt(1000);
+    			arr[i] = rand.nextInt(ARR_RANGE);
+    }
+    
+    /**
+     * swaps value at index i with value at index j in an array
+     * @param i
+     * @param j
+     * @param array
+     */
+    public void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    /**
+     * uses last element as pivot
+     * all elements that are smaller are put to the left
+     * all elements that are larger are put to the right
+     * @param arr
+     * @param low
+     * @param high
+     * @return
+     */
+    public int lomutoPartition(int low, int high) {
+        int pivot = arr[high]; 
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot) {
+                i++;
+                swap(i, j);
+            }
+        }
+        
+        swap(i+1, high);
+        
+        return i+1;
+    }
+    
+    /**
+     * recursive sorting function
+     * @param array to be sorted
+     * @param low: starting index
+     * @param high: ending index
+     * @return sorted array
+     */
+    public void lomutoSort(int low, int high) {
+    	
+    		if (low < high) {
+    			int pivot = lomutoPartition(low, high);
+    			
+    			//sorts elements before partition and after partition
+    			lomutoSort(low, pivot-1);
+    			lomutoSort(pivot+1, high);
+    		}
+    }
+    
+    /**
+     * uses middle element as pivot
+     * find an element from left side that is greater than the pivot
+     * and swap with element from right side that is smaller than the pivot
+     * @param low
+     * @param high
+     */
+    public void middleSort(int low, int high) {
+        
+        int i = low;
+        int j = high;
+       
+        int pivot = arr[(low + high)/ 2];
+        
+        //divide, search, and swap
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        
+        if (low < j)
+            middleSort(low, j);
+        if (i < high)
+            middleSort(i, high);
+    }
+    
+    public int getMeanIndex(int low, int high) {
+    	
+		int sum = 0, i, j;
+		for (i = low; i <= high; i++)
+			sum += arr[i];
+		
+		int mean = sum / (high - low + 1);
+		
+		int indexOfMean = low; //index of highest element under mean	
+		for (j = low; j <= high; j++) {
+			if (arr[j] > arr[indexOfMean] && arr[j] <= mean) indexOfMean = j;
+		}
+		
+		return indexOfMean;
+	}
+    
+    /**
+     * uses mean element as pivot
+     * find an element from left side that is greater than the pivot
+     * and swap with element from right side that is smaller than the pivot
+     * @param low
+     * @param high
+     */
+    public void meanSort(int low, int high) {
+        
+        int i = low;
+        int j = high;
+       
+        int pivot = arr[getMeanIndex(low, high)];
+        
+        //divide, search, and swap
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+            while (arr[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(i, j);
+                i++;
+                j--;
+            }
+        }
+        
+        if (low < j)
+            meanSort(low, j);
+        if (i < high)
+            meanSort(i, high);
+    }
+    
+    public String getRunTimes(String type) throws InterruptedException {
+    		String times = ""; //type;
+    		long startTime, endTime;
+    		double durationMilli;
+    		
+    		for (int i = 0; i < REPEATS; i++) {
+    			startTime = System.nanoTime();
+	        lomutoSort(0, ARR_LENGTH - 1);
+	        endTime = System.nanoTime();
+	        
+	        durationMilli = (endTime - startTime) / 1000000.0;
+	        times += (i+1) + ", " + durationMilli + ", ";
+	        
+	        startTime = System.nanoTime();
+	        middleSort(0, ARR_LENGTH - 1);
+	        endTime = System.nanoTime();
+	        
+	        durationMilli = (endTime - startTime) / 1000000.0;
+	        times += durationMilli + ", ";
+	        
+	        startTime = System.nanoTime();
+	        meanSort(0, ARR_LENGTH - 1);
+	        endTime = System.nanoTime();
+	        
+	        durationMilli = (endTime - startTime) / 1000000.0;
+	        times += durationMilli + "\n";
+	        
+	        Thread.sleep(100);
     		}
     		
-    		return array;
+    		return times;
+    		
+//    		for (int i = 0; i < REPEATS; i++) {
+//    			
+//    			ranArr();
+//				
+//    			if (type == LOMUTO) {
+//    		        startTime = System.nanoTime();
+//    		        lomutoSort(0, ARR_LENGTH - 1);
+//    		        endTime = System.nanoTime();
+//    			} else if (type == MIDDLE){
+//    		        startTime = System.nanoTime();
+//    		        middleSort(0, ARR_LENGTH - 1);
+//    		        endTime = System.nanoTime();
+//    			} else {
+//    		        startTime = System.nanoTime();
+//    		        meanSort(0, ARR_LENGTH - 1);
+//    		        endTime = System.nanoTime();
+//    			}
+//    			
+//    			double durationMilli = (endTime - startTime) / 1000000.0;  //divide by 1000000 to get milliseconds.
+//		        
+//	        System.out.println("Milliseconds: " + durationMilli + " Type: " + type);
+//	        times += ", " + durationMilli;
+//	 
+//	        System.out.println("sorted array");
+//	        printArray();
+//		}
+//    		
+//    		return times;
     }
+    
+    public static void main (String args[]) throws InterruptedException {
+    		
+    		Quicksort QS = new Quicksort();
+    		
+    		System.out.println(QS.getRunTimes(LOMUTO));
+    		System.out.println(QS.getRunTimes(MIDDLE));
+    		System.out.println(QS.getRunTimes(MEAN));
+    }
+    
 
 }
--- Quicksort/QuickSort/src/WriteCSV.java
+++ Quicksort/QuickSort/src/WriteCSV.java
@@ -9,7 +9,7 @@ public class WriteCSV {
 	public static String getFileHeader(int length) 
 	{
 		String fileHead = "Sort Type";
-		for (int i = 1; i <= 100; i++)
+		for (int i = 1; i <= Quicksort.REPEATS; i++)
 		{
 			fileHead += ", " + i;
 		}
@@ -17,24 +17,32 @@ public class WriteCSV {
 		return fileHead;
 	}
 	
-	public static void writeCSV(String[] times) {
+	public static void writeCSV() {
 		FileWriter fileWriter = null;
 	    
 	    try {
 		    	fileWriter = new FileWriter("/Users/Ziya/Documents/2017-2018/Quicksort/QuicksortTimes.csv");
 		    	
 		    	//Write the CSV file header
-		   fileWriter.append(getFileHeader(Quicksort.ARR_LENGTH));
+		    	fileWriter.append("Lomuto, Middle, Mean"); //getFileHeader(Quicksort.ARR_LENGTH));
+		   
+		    	Quicksort QS = new Quicksort();
+		   
+		    	Thread.sleep(1000);
 		   
 		    	//Add a new line separator after the header
 		    	fileWriter.append(NEW_LINE_SEPARATOR);
-		    fileWriter.append(LomutoQuicksort.runLoops(Quicksort.REPEATS));
+		    fileWriter.append(QS.getRunTimes(Quicksort.LOMUTO));
 		    
-		    fileWriter.append(NEW_LINE_SEPARATOR);
-		    fileWriter.append(MiddleQuicksort.runLoops(Quicksort.REPEATS));
-		    
-		    fileWriter.append(NEW_LINE_SEPARATOR);
-		    fileWriter.append(MeanQuicksort.runLoops(Quicksort.REPEATS));
+//		    Thread.sleep(1000);
+//		    
+//		    fileWriter.append(NEW_LINE_SEPARATOR);
+//		    fileWriter.append(QS.getRunTimes(Quicksort.MIDDLE));
+//		    
+//		    Thread.sleep(1000);
+//		    
+//		    fileWriter.append(NEW_LINE_SEPARATOR);
+//		    fileWriter.append(QS.getRunTimes(Quicksort.MEAN));
 		    
 		 	System.out.println("CSV file was created successfully");    
 	    } catch (Exception e) {
@@ -51,5 +59,9 @@ public class WriteCSV {
                 e.printStackTrace();
             }
         }
+	}
+	
+	public static void main (String args[]) {
+		writeCSV();
 	}
 }
